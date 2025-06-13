fca55b06ab9d62aa126999971e9edf23b8349cae
Shreyas <69378056+shreyaspranav@users.noreply.github.com>
2025-06-13 15:22:24 +0530

    chore: Added tabs for weather and news sections

---
+++ lib/tabs/news.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewsTab extends StatefulWidget {
+  const NewsTab({super.key});
+
+  @override
+  State<NewsTab> createState() => _NewsTabState();
+}
+
+class _NewsTabState extends State<NewsTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
---
+++ lib/tabs/weather.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherTab extends StatefulWidget {
+  const WeatherTab({super.key});
+
+  @override
+  State<WeatherTab> createState() => _WeatherTabState();
+}
+
+class _WeatherTabState extends State<WeatherTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
